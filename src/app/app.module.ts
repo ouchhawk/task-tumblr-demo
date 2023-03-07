0745a6eef2f32505032c4253840b882b608fd629
Ugur Aydogan <uguraydogan1@gmail.com>
2023-03-07 18:15:49 +0300

    Generate Navbar, Content and Footer Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
+import { ContentComponent } from './components/content/content/content.component';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ContentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
